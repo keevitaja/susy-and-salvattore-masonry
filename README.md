0aadc0069043592f52db72e2326e159028df7009
Tanel Tammik <keevitaja@gmail.com>
2015-03-25 18:11:32 +0200

    init this

---
+++ README.md
@@ -0,0 +1,4 @@
+# Responsive Masonry styled layout using Susy and Salvattore
+
+- http://salvattore.com/
+- http://susy.oddbird.net/
---
+++ config.rb
@@ -0,0 +1,9 @@
+require 'compass/import-once/activate'
+require 'susy'
+
+http_path = '/'
+css_dir = '/css'
+sass_dir = '/sass'
+
+output_style = :expanded
+preferred_syntax = :scss
